c142a885f99d6f5bc18538eba52c7234e8aab86d
DanielKriegerS <danielschifelbain@gmail.com>
2026-06-13 20:26:15 -0300

    Feat: implemented new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('../app/core/main/main/main').then(m => m.Main) },
   { path: 'quarters', loadComponent: () => import('../app/pages/quarters/quarters').then(m => m.QuartersComponent) },
   { path: 'quarters/:id', loadComponent: () => import('../app/pages/quarter/quarter').then(m => m.QuarterComponent) },
-  { path: 'sprints', loadComponent: () => import('../app/pages/sprints/sprints').then(m => m.SprintComponent) },
+  { path: 'sprints', loadComponent: () => import('../app/pages/sprints/sprints').then(m => m.SprintsComponent) },
   { path: 'sprints/:id', loadComponent: () => import('../app/pages/sprint/sprint').then(m => m.SprintComponent) },
+  { path: 'coatends', loadComponent: () => import('../app/pages/coatends/coatends').then(m => m.CoatendsComponent) },
   { path: 'coatends/:id', loadComponent: () => import('../app/pages/coatend/coatend').then(m => m.CoatendComponent) },
   { path: '**', redirectTo: '' }
 ];
